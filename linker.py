73f3ed9fdaa087a7d27fff886fe70540f96fc64e
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-15 23:43:20 +0400

    Added linkFile method and refactoring

--- linker.py
+++ linker.py
@@ -12,7 +12,7 @@ linkedDirs = {
 
 #LINKER METHOD
 def toLink(dir, file):
-    outCodeString = ''      #output code string which will be placed into ouput file
+    outCodeString = ''      #output 0615code string which will be placed into ouput file
     if file != 'linker.py' and file!= 'main.js':
         openFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')      #open file for link
         outCodeString += openFile.read()+'\n'                                       #add to output code string new code from linked file
@@ -20,14 +20,17 @@ def toLink(dir, file):
     outFile.write(outCodeString)
 
 
-for dir in linkedDirs.values():             #iteration for all directories in dirs{}
-    tree = os.walk(dir)           #make file tree for directories inside main directories (dirs{})
-    for subDir in tree:             #iteration for all destination directories in tree
-        for finalFile in subDir[2]:                         #iteration for all destination files
-            fileExtension = finalFile.split('.')[1]       #split file name to name and extension for '.'
-            #link files with only "js" destination
-            if(fileExtension == "js"):
-                toLink(subDir[0], finalFile)
-                print(str(subDir[0])+'/'+ str(finalFile))
+def linkFile(dir, file):
+    fileExtension = file.split('.')[1]       #split file name to name and extension for '.'
+    #link files with only "js" destination
+    if(fileExtension == "js"):
+        toLink(subDir[0], file)
+        print(str(dir)+'/'+str(file))
+
+
+for dir in linkedDirs.values():             #iteration for all directories in linkedDirs{}
+    for subDir in os.walk(dir):             #iteration for all subdirectories
+        for finalFile in subDir[2]          #iteration for all destination files
+            linkFile(dir, finalFile)
 
 outFile.close()         #close output file
