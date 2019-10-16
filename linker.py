89d5b92a328685e6eeca1b691feb0eaba9b3c2df
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-16 20:21:58 +0400

    Refactoring

--- linker.py
+++ linker.py
@@ -1,6 +1,24 @@
 import os
-from pathlib import Path
 
+def linker():
+    outPutFile = open('main.js', 'w', encoding = 'UTF-8')
+
+    # TODO: transfer to json
+    # directories in witch files for linking are located
+    linkedDirs = {
+        "engine" : './engine',
+        "graphics" : './graphics',
+        "game" : './game'
+    }
+
+    for dir in linkedDirs.values():              # iteration for all directories in linkedDirs{}
+        for subDir in os.walk(dir):              # iteration for all subdirectories
+            for finalFile in subDir[2]:          # iteration for all destination files
+                filePath = getFilePathForDirAndName(dir, finalFile)
+                linkFinalFileWithOutPutFile(filePath, outPutFile)
+                printRelativePathForFile(filePath)
+
+    outPutFile.close()         # close output file
 
 
 
@@ -8,13 +26,12 @@ def getFilePathForDirAndName(dir, file):
     return str(str(dir) + '/' + str(file))
 
 
-def linkFile(filePath):
+def linkFinalFileWithOutPutFile(filePath, outPutFile):
     if( (getFileExtension(filePath) == ".js") and (os.path.basename(filePath) != 'main.js') ):
-        toLink(filePath)
+        outPutFile.write(getTextFromFile(filePath))
 
 def getFileExtension(filePath):
     return Path(filePath).suffix
-
 
 def getTextFromFile(filePath):
     linkableFile = open(filePath, mode = 'r',encoding = 'UTF-8')
@@ -22,31 +39,8 @@ def getTextFromFile(filePath):
     linkableFile.close()
     return codeString
 
-#LINKER METHOD
-def toLink(filePath):
-    outFile.write(getTextFromFile(filePath))
-
 
 def printRelativePathForFile(filePath):
     print(filePath)
 
-
-
-#output file
-outFile = open('main.js', 'w', encoding = 'UTF-8')
-
-#dictionary of start directories of link files
-linkedDirs = {
-    "engine" : './engine',
-    "graphics" : './graphics',
-    "game" : './game'
-}
-
-for dir in linkedDirs.values():             #iteration for all directories in linkedDirs{}
-    for subDir in os.walk(dir):             #iteration for all subdirectories
-        for finalFile in subDir[2]:          #iteration for all destination files
-            filePath = getFilePathForDirAndName(dir, finalFile)
-            linkFile(filePath)
-            printRelativePathForFile(filePath)
-
-outFile.close()         #close output file
+linker()
