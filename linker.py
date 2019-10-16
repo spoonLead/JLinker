9e1b50c13eaad02c59bb782aca966022f47a4c87
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-16 11:16:47 +0400

    Refactoring arguments

--- linker.py
+++ linker.py
@@ -2,28 +2,31 @@ import os
 from pathlib import Path
 
 
+
+
+def getFilePath(dir, file):
+    return str(str(dir) + '/' + str(file))
+
+
+def linkFile(filePath):
+    if(getFileExtension(filePath) == ".js"):
+        toLink(filePath)
+
+def getFileExtension(filePath):
+    return Path(filePath).suffix
+
 #LINKER METHOD
-def toLink(dir, file):
+def toLink(filePath):
     outCodeString = ''      #output 0615code string which will be placed into ouput file
-    if file != 'linker.py' and file!= 'main.js':
-        linkableFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')      #open file for link
+    if os.path.basename(filePath) != 'main.js':
+        linkableFile = open(filePath, mode = 'r',encoding = 'UTF-8')      #open file for link
         outCodeString += linkableFile.read()+'\n'                                       #add to output code string new code from linked file
         linkableFile.close()                                                            #close file for link
     outFile.write(outCodeString)
 
 
-
-def linkFile(dir, file):
-    if(getFileExtension(file) == ".js"):
-        toLink(subDir[0], file)
-
-def getFileExtension(file):
-    return Path(file).suffix
-
-
-
-def printRelativePathForFile(dir, file):
-    print(str(dir)+'/'+str(file))
+def printRelativePathForFile(filePath):
+    print(filePath)
 
 
 
@@ -40,7 +43,8 @@ linkedDirs = {
 for dir in linkedDirs.values():             #iteration for all directories in linkedDirs{}
     for subDir in os.walk(dir):             #iteration for all subdirectories
         for finalFile in subDir[2]:          #iteration for all destination files
-            linkFile(dir, finalFile)
-            printRelativePathForFile(dir, finalFile)
+            filePath = getFilePath(dir, finalFile)
+            linkFile(filePath)
+            printRelativePathForFile(filePath)
 
 outFile.close()         #close output file
