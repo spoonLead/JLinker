609a55f9cd7d73d547bc59acf103e22aa8ca487f
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-16 09:35:26 +0400

    Added printRelativePathForFile method

--- linker.py
+++ linker.py
@@ -1,6 +1,32 @@
 import os
 from pathlib import Path
 
+
+#LINKER METHOD
+def toLink(dir, file):
+    outCodeString = ''      #output 0615code string which will be placed into ouput file
+    if file != 'linker.py' and file!= 'main.js':
+        linkableFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')      #open file for link
+        outCodeString += linkableFile.read()+'\n'                                       #add to output code string new code from linked file
+        linkableFile.close()                                                            #close file for link
+    outFile.write(outCodeString)
+
+
+
+def linkFile(dir, file):
+    if(getFileExtension(file) == ".js"):
+        toLink(subDir[0], file)
+
+def getFileExtension(file):
+    return Path(file).suffix
+
+
+
+def printRelativePathForFile(dir, file):
+    print(str(dir)+'/'+str(file))
+
+
+
 #output file
 outFile = open('main.js', 'w', encoding = 'UTF-8')
 
@@ -11,29 +37,10 @@ linkedDirs = {
     "game" : './game'
 }
 
-#LINKER METHOD
-def toLink(dir, file):
-    outCodeString = ''      #output 0615code string which will be placed into ouput file
-    if file != 'linker.py' and file!= 'main.js':
-        openFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')      #open file for link
-        outCodeString += openFile.read()+'\n'                                       #add to output code string new code from linked file
-        openFile.close()                                                            #close file for link
-    outFile.write(outCodeString)
-
-
-def linkFile(dir, file):
-    fileExtension = getFileExtension(file)
-    #link files with only "js" destination
-    if(fileExtension == ".js"):
-        toLink(subDir[0], file)
-        print(str(dir)+'/'+str(file))
-
-def getFileExtension(file):
-    return Path(file).suffix
-
 for dir in linkedDirs.values():             #iteration for all directories in linkedDirs{}
     for subDir in os.walk(dir):             #iteration for all subdirectories
         for finalFile in subDir[2]:          #iteration for all destination files
             linkFile(dir, finalFile)
+            printRelativePathForFile(dir, finalFile)
 
 outFile.close()         #close output file
